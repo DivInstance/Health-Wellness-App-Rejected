0fbb940cb8caced3862cfe26cb0194310e1f0e5a
= <=>
2024-11-15 01:52:58 +0530

    Integrate the registration, logout, edit profile and download profile features to the backend.

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,32 +2,42 @@ import JWT from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
 //User Authentication
-export const isAuthenticated = async (req,res,next) => {
+export const isAuthenticated = async (req, res, next) => {
     //descrtucture token from cookie
-    const {token} = req.cookies;
-    console.log("hi")
-    console.log(req)
+    
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
 
-    //validation
-    if(!token) {        
-        return res.status(401).send({
-            success: false,
-            message: 'Unauthorized user access'
-        })
-    }
+  // Extract token
+  const token = authHeader.split(" ")[1]; // Get the token after 'Bearer'
+  console.log("hi");
+  // console.log(req)
+
+  //validation
+  if (!token) {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized user access",
+    });
+  }
 
     const decodeData = JWT.verify(token, process.env.JWT_SECRET);
-    req.user = await userModel.findById(decodeData._id);
-    next();
-}
+    // console.log(decodeData)
+    
+    req.user = await userModel.findById(decodeData._id).select("-password");
+    // console.log(req.user);
+  next();
+};
 
 //Admin Authentication
-export const isAdmin = async (req,res,next)=>{
-    if(req.user.role!='admin'){
-        return res.status(403).send({
-            success: false,
-            message: 'Unauthorized admin access'
-        })
-    }  
-    next();
-}
+export const isAdmin = async (req, res, next) => {
+  if (req.user.role != "admin") {
+    return res.status(403).send({
+      success: false,
+      message: "Unauthorized admin access",
+    });
+  }
+  next();
+};

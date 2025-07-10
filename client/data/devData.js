6d6c0c0484811ba817bd94956a2cb57c8c5cb269
Divyaranjan Sahoo <divyaranjan20175@gmail.com>
2025-07-11 00:28:39 +0530

    Test feature

--- client/data/devData.js
+++ client/data/devData.js
@@ -17,7 +17,7 @@ export const devData = [
 
         id:3,
         title: "Divyaranjan Sahoo",
-        description: "Tester of Health Wellness Application",
+        description: "Application Tester",
         image : "https://static.wixstatic.com/media/8fa58b_ae395bf438b04c9dad1097955f30b70b~mv2.jpg/v1/fill/w_370,h_444,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/IMG_20231221_230656_472%20-%203363%20KHUSHI.jpg"
         
     }
